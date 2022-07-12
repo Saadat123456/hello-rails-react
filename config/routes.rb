cca5ae3864aff1d5596c44e711b5f94a90478ef7
Saadat Ali <saadatlucky313@gmail.com>
2022-07-12 18:34:57 +0500

    Add static route

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,9 @@ Rails.application.routes.draw do
   namespace :v1, defaults: { format: 'json' } do
     get 'greeting', to: 'greetings#index'
   end
+
+  get '*page', to: 'static#index', constraints: -> (req) do
+    !req.xhr? && req.format.html?
+  end
+  root "static#index"
 end
